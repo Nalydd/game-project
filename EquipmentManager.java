0d67ed892c9ba6a379308e4e5657c25469c4c12a
twarren51 <71302764+twarren51@users.noreply.github.com>
2020-10-14 21:33:35 -0400

    Add files via upload

---
+++ EquipmentManager.java
@@ -0,0 +1,79 @@
+import java.util.* ;
+
+public class EquipmentManager
+{
+    private ArrayList<Plackart> plackarts = new ArrayList<Plackart>();
+    private ArrayList<Helmet> helmets = new ArrayList<Helmet>();
+    private ArrayList<LongBow> longBows = new ArrayList<LongBow>();
+    private ArrayList<ThrowingAxe> throwingAxes = new ArrayList<Plackart>();
+    private ArrayList<Spear> spears = new ArrayList<Spear>();
+    private ArrayList<Sword> swords = new ArrayList<Sword>();
+    public EquipmentManager()
+    {
+
+    }
+    public void clearAll()
+    {
+
+    }
+    public int countEquipment()
+    {
+        return 0;
+    }
+    public int countArmor()
+    {
+        return 0;
+    }
+    public int countWeapon()
+    {
+        return 0;
+    }
+    public void removeEquipment(String list, int index)
+    {
+
+    }
+    public String getEquipmentDetails(String list, int index)
+    {
+        return"";
+    }
+    public String getEquipmentList()
+    {
+        return "";
+    }
+    public String getEquipmentListDetails()
+    {
+        return "";
+    }
+    public void addPlackart(Plackart p)
+    {
+
+    }
+    public void addHelmet(Helmet h)
+    {
+
+    }
+    public void addLongBow(LongBow b)
+    {
+
+    }
+    public void addThrowingAxe(ThrowingAxe a)
+    {
+
+    }
+    public void addSpear(Spear s)
+    {
+
+    }
+    public void addSword(LongBow s)
+    {
+
+    }
+    private void removeEquipment(ArrayList list, int index)
+    {
+
+    }
+    private void getEquipmentDetails(ArrayList list, int index)
+    {
+
+    }
+}
