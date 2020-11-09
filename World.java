6bc9477e5de0b5fc8d8f24315ff9163d624a6c7c
Seth Gilstrap <gilstraps@etsu.edu>
2020-11-09 01:58:40 -0500

    Created the World Class
    Uploading the base code for the World Class

---
+++ World.java
@@ -0,0 +1,89 @@
+public class World {
+
+    public Arraylist<Chest> chest = new ArrayList<Chest>();
+    public ArrayList<Barrel> barrel = new ArrayList<Barrel>();
+    public ArrayList<Person> person = new ArrayList<Person>();
+    public Person player;
+
+    public World()
+    {
+
+    }
+
+    public void makeChest()
+    {
+
+    }
+
+    public void makeBarrel()
+    {
+
+    }
+
+    public void makeHuman()
+    {
+
+    }
+
+    public void makeGoblin()
+    {
+
+    }
+
+    public String listChests()
+    {
+
+    }
+
+    public String listBarrels()
+    {
+
+    }
+
+    public String listPeople()
+    {
+
+    }
+
+    public String listTargets()
+    {
+
+    }
+
+    public String speakTo(person Person)
+    {
+
+    }
+
+    public int attack(target IHitable)
+    {
+
+    }
+
+    public void transferEquipment(source IInventory, destination IInventory)
+    {
+
+    }
+
+    public String peakInside(inventory IInventory)
+    {
+
+    }
+
+    public String getPersonInfo(person Person)
+    {
+
+    }
+
+    public String getPlayerInfo()
+    {
+
+    }
+
+    public String listPlayerInventory()
+    {
+
+    }
+
+    public boolean equipEquipment()
+}
